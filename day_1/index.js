d1bb36809d47caf034c183bfea7524f2e7be2873
danangace <danangbahari89@gmail.com>
2021-04-15 15:05:26 +0700

    first commit

---
+++ day_1/index.js
@@ -0,0 +1,13 @@
+// directive fitur2 untuk handling event html / javascript
+// input, submit, list (looping), condition
+
+// v-if
+// v-else
+// v-else-if
+// v-for
+// v-model
+// v-on
+// v-html
+
+// module => component and depth
+// v-bind => komunikasi antara parent dan child
